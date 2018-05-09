d725f033cb9706bfea25d15534060aca1b6d864c
bhague1281 <bhague1281@gmail.com>
2018-05-08 23:36:23 -0700

    100% code coverage of Counter

--- src/Counter/Counter.test.js
+++ src/Counter/Counter.test.js
@@ -12,4 +12,14 @@ describe('Counter', () => {
         wrapper.find('.Counter__button--increment').simulate('click').simulate('click');
         expect(wrapper).toMatchSnapshot();
     });
+
+    it('Should decrement when clicking on the decrement button', () => {
+        const wrapper = mount(<Counter />);
+
+        // console.log(wrapper.find('button'));
+
+        expect(wrapper.find('button')).toHaveLength(2);
+        wrapper.find('.Counter__button--decrement').simulate('click').simulate('click');
+        expect(wrapper).toMatchSnapshot();
+    });
 });
