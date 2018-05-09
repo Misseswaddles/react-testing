4e9ebe3e3873adc9eaecd717b08336cc67967aef
bhague1281 <bhague1281@gmail.com>
2018-05-08 23:19:59 -0700

    React testing with enzyme

---
+++ src/Counter/Counter.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Counter from './Counter';
+
+describe('Counter', () => {
+    it('Should increment when clicking on the increment button', () => {
+        const wrapper = mount(<Counter />);
+
+        // console.log(wrapper.find('button'));
+
+        expect(wrapper.find('button')).toHaveLength(2);
+        wrapper.find('.Counter__button--increment').simulate('click').simulate('click');
+        expect(wrapper).toMatchSnapshot();
+    });
+});
